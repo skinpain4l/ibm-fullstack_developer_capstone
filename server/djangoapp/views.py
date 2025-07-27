9a04a85710b621fa5ed20649d8b6f7bc682b485e
skinpain4l <sydomn90872@gmail.com>
2025-07-27 02:33:09 -0400

    Add CarMake and CarModel Django models and register them with admin

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,6 +14,8 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+# Import CarMake and CarModel models
+from .models import CarMake, CarModel
 
 
 # Get an instance of a logger
@@ -94,3 +96,14 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+# Get the list of cars
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
